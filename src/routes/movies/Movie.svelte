a28a51c7ebfb15777fa1492c20df370e7f2051f7
lbabino <lucasbabino@gmail.com>
2023-01-20 10:58:26 -0300

    movie component

---
+++ src/routes/movies/Movie.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import {baseImgUrl, filesizes} from '../../env'
+	import type { MovieSchema } from './types';
+
+    export let movie:MovieSchema;
+</script>
+
+<h3>
+    <u>{movie.title}</u> ({movie.release_date.slice(0, 4)})({movie.original_language})
+</h3>
+<img
+    on:keydown
+    loading="lazy"
+    width="300px"
+    height="450px"
+    src={`${baseImgUrl}/${filesizes[1]}/${movie.poster_path}`}
+    alt={movie.title}
+/>
+<p>{movie.overview}</p>
+<a href={'/movie/' + movie.id}><i>More details...</i></a>
+
+<style>
+</style>
