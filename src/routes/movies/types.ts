70f1ae1e21b9ce5219afbcd32accdc1488237bb6
lbabino <lucasbabino@gmail.com>
2023-01-20 16:57:01 -0300

    added type

--- src/routes/movies/types.ts
+++ src/routes/movies/types.ts
@@ -1,27 +1,53 @@
 export type Genre = {
-    id:number,
-    name:string
-}
+	id: number;
+	name: string;
+};
 
 export type Selectable = {
-    selected:boolean
-}
+	selected: boolean;
+};
 
 export type MovieSchema = {
-    "adult": boolean,
-    "backdrop_path": string,
-    "genre_ids": number[],
-    "id": number,
-    "original_language": string,
-    "original_title": string,
-    "overview": string,
-    "popularity": number,
-    "poster_path": string,
-    "release_date": string,
-    "title": string,
-    "video": boolean,
-    "vote_average": number,
-    "vote_count": number
-}
+	adult: boolean;
+	backdrop_path: string;
+	genre_ids: number[];
+	id: number;
+	original_language: string;
+	original_title: string;
+	overview: string;
+	popularity: number;
+	poster_path: string;
+	release_date: string;
+	title: string;
+	video: boolean;
+	vote_average: number;
+	vote_count: number;
+};
 
-export type SelectableGenre = Genre & Selectable
+export type MovieDetailsSchema = {
+	adult: boolean;
+	backdrop_path: string;
+	belongs_to_collection: object;
+	budget: number;
+	genres: number[];
+	homepage: string;
+	id: number;
+	imdb_id: string;
+	original_language: string;
+	original_title: string;
+	overview: string;
+	popularity: number;
+	poster_path: string;
+	release_date: string;
+	revenue: number;
+	runtime: number;
+	status: string;
+	tagline: string;
+	title: string;
+	video: boolean;
+	vote_average: number;
+	vote_count: number;
+    
+};
+
+export type SelectableGenre = Genre & Selectable;
